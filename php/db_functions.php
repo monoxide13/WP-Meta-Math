7304f544c1f8a5c6f3be2dff2bbc7e7358f0f80a
monoxide13 <git@monoxide13.com>
2020-06-21 13:12:17 -0500

    Cleaned up db functions. Added edge case testings to shortcodes.

--- php/db_functions.php
+++ php/db_functions.php
@@ -3,20 +3,6 @@
 require_once('includes.php');
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
-function mm_resolveSlug($slug){
-	global $wpdb;
-	if($slug=='')
-		return 0;
-	$result = $wpdb->get_col("SELECT term_id FROM {$wpdb->prefix}terms WHERE slug LIKE '$slug'", 0);
-	if($result===false){
-		error_log("DB ERROR: ".__file__.__line__);
-		return 0;
-	}elseif($result==false){
-		return 0;
-	}
-	return $result[0];
-}
-
 function mm_getTermTaxIDsByTax($taxonomy){
 	global $wpdb;
 	if($taxonomy==null || $taxonomy=='')
@@ -26,15 +12,15 @@ function mm_getTermTaxIDsByTax($taxonomy){
 		return null;
 	return $results;
 }
-function mm_getTermTaxIDBySlug($slug){
+
+function mm_getTermTaxIDBySlug($taxonomy, $slug){
 	global $wpdb;
-	$slugid = mm_resolveSlug($slug);
-	$result = $wpdb->get_col("SELECT tt.term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy tt LEFT JOIN {$wpdb->prefix}terms trm on tt.term_id=trm.term_id WHERE trm.slug LIKE '$slug'",0);
+	$result = $wpdb->get_col("SELECT tt.term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy tt LEFT JOIN {$wpdb->prefix}terms trm on tt.term_id=trm.term_id WHERE tt.taxonomy LIKE '$taxonomy' AND trm.slug LIKE '$slug';",0);
 	return $result[0];
 }
 function mm_getTaxonomyChildren($parent){
 	global $wpdb;
-	$result = $wpdb->get_col("SELECT term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy WHERE parent=$parent", 0);
+	$result = $wpdb->get_col("SELECT term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy WHERE parent=$parent;", 0);
 	if($result===false){
 		error_log("DB ERROR: ".__file__.__line__);
 		$output[] = $parent;
@@ -50,10 +36,6 @@ function mm_getTaxonomyChildren($parent){
 }
 
 function mm_sumMeta($taxIDs, $meta_key){
-	if($meta_key==null){
-		hit_log('Meta_key null');
-		return null;
-	}
 	global $wpdb;
 	$taxString='';
 	if($taxIDs!=null){
@@ -61,9 +43,7 @@ function mm_sumMeta($taxIDs, $meta_key){
 		$taxString .= implode(', ', $taxIDs);
 		$taxString .= ')';
 	}
-	hit_log('1');
 	$query="SELECT tr.term_taxonomy_id, pm.post_id, pm.meta_key, pm.meta_value FROM {$wpdb->prefix}postmeta pm JOIN {$wpdb->prefix}term_relationships tr ON pm.post_id=tr.object_id WHERE pm.meta_key LIKE '$meta_key'$taxString;";
-	hit_log($query);
 	$result = $wpdb->get_col($query, 3);
 	return $result;
 }
--- php/shortcodes.php
+++ php/shortcodes.php
@@ -5,18 +5,19 @@ function meta_math_shortcode($atts){
 	$atts = shortcode_atts(
 		array(
 			'taxonomy' => 'section', // Have an option to set default taxonomy
-			'slug' => 'empennage',
+			'slug' => '',
 			'meta' => '',
 			'math' => 'sum',
 		), $atts, 'meta-math');
 	if($atts['meta'] == '')
 		return 'META NOT DEFINED';
-	if('taxonomy'==''){
-		$term_taxonomy_ids = null;
-	}elseif($atts['slug']==''){
+	if($atts['taxonomy'] == '')
+		return 'TAXONOMY NOT DEFINED';
+	if($atts['slug'] == ''){
 		$term_taxonomy_ids = mm_getTermTaxIDsByTax($atts['taxonomy']);
 	}else{
-		$term_taxonomy_ids = mm_getTaxonomyChildren(mm_getTermTaxIDBySlug($atts['slug']));
+		$term_taxonomy_ids = mm_getTaxonomyChildren(mm_getTermTaxIDBySlug($atts['taxonomy'],$atts['slug']));
+		hit_log($atts['taxonomy'].":".implode(',',$term_taxonomy_ids));
 	}
 	$result = mm_sumMeta($term_taxonomy_ids, $atts['meta']);
 	return array_sum($result);
