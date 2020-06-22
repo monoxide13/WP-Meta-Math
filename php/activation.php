58132d6705e04ededaa0b478f91e484f89afbcdc
monoxide13 <git@monoxide13.com>
2020-06-21 20:09:55 -0500

    Fixed activation.

--- php/activation.php
+++ php/activation.php
@@ -1,12 +1,13 @@
 <?php
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
 
 function meta_math_activation(){
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
 	// Register settings
 	$options['meta_math_posttype_field']='';
 	$options['meta_math_taxonomy_field']='';
 	update_option(meta_math_settings, $options);
 }
+
 register_activation_hook(MM_PLUGIN_LOCATION, 'meta_math_activation' );
 
 function meta_math_deactivation(){
